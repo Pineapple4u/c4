be79c47cec5985d5b15bc3088a3986c3d2108648
Pineapple4u <165797715+Pineapple4u@users.noreply.github.com>
2024-09-03 17:30:29 +0530

    Add files via upload

---
+++ javasc.js
@@ -0,0 +1,39 @@
+function sum(n1, n2)
+{
+ console.log(n1+n2);
+}
+
+function min(n1,n2)
+{
+    console.log(n1-n2);
+}
+
+function mul(n1, n2)
+{
+    console.log(n1*n2);
+}
+
+function divi(n1, n2)
+{
+    console.log(n1/n2);
+}
+
+function mod(n1,n2)
+{
+    console.log(n1%n2);
+}
+
+sum(12,7);
+min(283, 193);
+mul(563, 458);
+divi(1236, 99);
+mod(1236, 99);
+
+
+
+var displayName = function (name1, name2){
+return name1+" "+name2
+}
+
+var name= displayName("Aarna", "Kapuria");
+console.log(name)
